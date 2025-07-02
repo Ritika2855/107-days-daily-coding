25b6cf5ad735fa3f34aa20eb33dea48243e824bd
Ritika <ritijais285@gmail.com>
2025-07-03 02:17:10 +0530

    added

---
+++ first.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int age=25;
+    char ch;
+    char grade='A';
+    char bb='a';
+    int yy=bb;
+    cout << yy <<"\n";
+    int value=grade;
+    cout << value;
+    float PI=3.14F;
+    bool isSafe=false;
+    cout <<"\n"<< sizeof(age)<<"\n" << "hello\n" << grade << "\n";
+    cout << sizeof(ch) << "\n" << PI;
+    cout <<"\ncoding with ritika\nBegin her journey on c++"<<" let's encourage her\n"<< isSafe;
+    cout <<"\nthis is my first cpp project";
+    return 0;
+}
